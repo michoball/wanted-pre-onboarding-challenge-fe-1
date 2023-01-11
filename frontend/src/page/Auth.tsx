56e71c71e91933083a0ff6b97dc2ca0c4a6f346e
michoball <myunghun0114@gmail.com>
2023-01-11 20:35:04 +0900

    feat : Add Sign up component to auth route

--- frontend/src/page/Auth.tsx
+++ frontend/src/page/Auth.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Login from "../components/Login";
+import SignUp from "../components/SignUp";
 
 const Auth = () => {
-  return <Login />;
+  return (
+    <div style={{ display: "flex", gap: "20px" }}>
+      <Login />
+      <SignUp />
+    </div>
+  );
 };
 
 export default Auth;
