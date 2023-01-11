1bbbf3ce6a00d4b5b94b030772cb6c406b0c2253
michoball <myunghun0114@gmail.com>
2023-01-11 20:32:18 +0900

    feat : Add sending user token to todo context function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AuthContext from "./context/authContext";
+import TodoContext from "./context/todoContext";
 import Auth from "./page/Auth";
 import Home from "./page/Home";
 import PrivateRoute from "./page/PrivateRoute";
 import Todo from "./page/Todo";
 
 function App() {
-  const { isLoggedIn } = useContext(AuthContext);
+  const { isLoggedIn, user } = useContext(AuthContext);
+  const { setUserToken } = useContext(TodoContext);
+
+  useEffect(() => {
+    if (user) {
+      setUserToken(user.token);
+    }
+  }, [user, setUserToken]);
 
   return (
     <Routes>
