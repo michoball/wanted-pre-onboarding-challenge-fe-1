72665935ab3e37cc5396dbcf3ff7241d920a1381
michoball <myunghun0114@gmail.com>
2023-01-16 00:44:42 +0900

    feat : Apply react-query

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext";
 import { TodoProvider } from "./context/todoContext";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthContextProvider>
-        <TodoProvider>
-          <App />
-        </TodoProvider>
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <AuthContextProvider>
+          <TodoProvider>
+            <App />
+          </TodoProvider>
+        </AuthContextProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
